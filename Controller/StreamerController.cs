409d1eb03702b4a7dafc8cc41793dbbc4458ccf2
laurence.habdas <laurence.habdas@gmail.com>
2018-05-23 13:48:21 +0200

    initial commit

---
+++ Controller/StreamerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom.Controller
+{
+    public class StreamerController : ControllerBase
+    {
+        private readonly IHubContext<Chat> context;
+
+        public StreamerController(IHubContext<Chat> context)
+        {
+            this.context = context;
+        }
+
+        [Route("/api/test/")]
+        [HttpGet]
+        public void Test(string id)
+        {
+            Task.Run(async () =>
+            {
+                for (var i=1;i<1000000;i++)
+                {
+                    await context.Clients.Group("test").SendAsync("broadcastMessage", "stremaer", $"this is from the streamer { DateTime.Now }, current beat is {i}").ConfigureAwait(false);
+                    await Task.Delay(50).ConfigureAwait(false);
+                }
+
+            });
+        }
+
+    }
+}
---
+++ Hub/Chat.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Chat : Hub
+    {
+        private readonly IHubContext<Chat> hubContext;
+
+        public Chat(IHubContext<Chat> hubContext) :base()
+        {
+            this.hubContext = hubContext;
+        }
+      
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+
+
+        public void AddGroup()
+        {
+           this.hubContext.Groups.AddToGroupAsync(Context.ConnectionId, "test");
+        }
+    }
+}
